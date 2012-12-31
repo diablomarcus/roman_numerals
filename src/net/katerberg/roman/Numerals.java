b5877993694a54255b83d12e3020e980d565a1b3
diablomarcus <diablomarcus@gmail.com>
2012-12-30 22:38:24 -0600

    Updated javadocs for Numerals. I wish this were cleaner. Dates suck

--- src/net/katerberg/roman/Numerals.java
+++ src/net/katerberg/roman/Numerals.java
@@ -3,6 +3,10 @@ package net.katerberg.roman;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author diablo
+ *
+ */
 public class Numerals {
 
 	private static Map<String, Integer> _INSTANCE;
@@ -12,6 +16,11 @@ public class Numerals {
 	
 	//This will always be a bidirectional map. I super-promise. We'll ensure this with BiDiMap when I get internet access.
 	//TODO: Make this bidirectional.
+	/**
+	 * Singleton map of available Latin numeral representations.
+	 * 
+	 * @return One-to-One map of valid Latin numeric representations to their Arabic counterparts
+	 */
 	public synchronized static Map<String, Integer> getInstance(){
 		if (null == _INSTANCE) {
 			createNewInstance();
@@ -19,6 +28,13 @@ public class Numerals {
 		return _INSTANCE;
 	}
 	
+	/**
+	 * Checks to see if <tt>testedString</tt> passes basic sanity tests based on the rules
+	 * provided in Processor.
+	 * 
+	 * @param testedString Numeral to run basic tests against
+	 * @return			   True if numeral passes sanity tests. False if not.
+	 */
 	public synchronized static boolean isValidNumeral(String testedString){
 		//Make sure it's not empty
 		if (null == testedString || testedString.isEmpty()) {
@@ -45,6 +61,10 @@ public class Numerals {
 		return true;
 	}
 
+	
+	/**
+	 * Creates and populates the map of valid Latin-to-Arabic numeric representations
+	 */
 	private static void createNewInstance() {
 		_INSTANCE=new HashMap<String, Integer>();
 		_INSTANCE.put("I", 1);
